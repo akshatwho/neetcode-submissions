017f53fce8c98c995c91eb7138218f3739ec065f
akshatwho <55390158+akshatwho@users.noreply.github.com>
2026-07-10 21:31:14 +0100

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+                System.out.println(nums[i]);
+
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        if (goal == 0) {
+            return true;
+        }
+
+        return false;
+    }
+}
